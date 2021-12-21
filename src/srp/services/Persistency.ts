8e517c4b78523c93e6e2c1d7d94c770caba8ef32
Richardson Bruno <richardsonbrc.ads@gmail.com>
2021-12-21 19:16:12 -0300

    fix: change return console log inner

--- src/srp/services/Persistency.ts
+++ src/srp/services/Persistency.ts
@@ -1,6 +1,6 @@
 import { IPersistency } from '../entities/interfaces/IPersistency';
 export class Persistency implements IPersistency {
   saveOrder():void {
-    console.log('Salvo com sucesso');
+    console.log('Salvo com sucesso...');
   }
 }
